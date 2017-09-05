609614693dfb372ca4765aa3de1aa5720c34e764
Stefano Allesina <sallesina@uchicago.edu>
2017-09-05 17:54:19 -0500

    contact info for workbook

--- workbook/compile_workbook/compile_workbook.sh
+++ workbook/compile_workbook/compile_workbook.sh
@@ -5,8 +5,8 @@ pdftk ../other_docs/cover.pdf ../other_docs/blank.pdf cat output tmp.pdf
 pdftk tmp.pdf ../other_docs/map.pdf cat output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Contact Info # Revised
-pandoc ../other_docs/contact_info.md -o ../other_docs/contact2.pdf
-gs -o ../other_docs/contact2.pdf -dNoOutputFonts -sDEVICE=pdfwrite ../other_docs/contact2.pdf
+pandoc ../other_docs/contact_info.md -o ../other_docs/contact.pdf
+gs -o ../other_docs/contact.pdf -dNoOutputFonts -sDEVICE=pdfwrite ../other_docs/contact2.pdf
 pdftk tmp.pdf ../other_docs/contact2.pdf cat output tmp2.pdf; mv tmp2.pdf tmp.pdf
 
 ## Block Room Schedule (General)
